5d8ff1e43568929c6e14fea19fe1ee4a8596bd64
benrich <benrichprince1@gmail.com>
2025-07-04 12:29:10 +0100

    Fix routing to support subfolder API paths and update register.html AJAX URL

--- public/index.php
+++ public/index.php
@@ -5,9 +5,17 @@ require_once __DIR__ . '/../src/controllers/BookController.php';
 require_once __DIR__ . '/../src/middleware/AuthMiddleware.php';
 require_once __DIR__ . '/../src/controllers/ReviewController.php';
 
-// Basic routing skeleton
+// Extract the path after /public for routing
 $requestUri = $_SERVER['REQUEST_URI'];
 $requestMethod = $_SERVER['REQUEST_METHOD'];
+$scriptName = dirname($_SERVER['SCRIPT_NAME']);
+$path = parse_url($requestUri, PHP_URL_PATH);
+if (strpos($path, $scriptName) === 0) {
+    $path = substr($path, strlen($scriptName));
+}
+if ($path === false || $path === '') {
+    $path = '/';
+}
 
 $bookIdPattern = '#^/books/([a-fA-F0-9]{24})$#';
 $reviewIdPattern = '#^/reviews/([a-fA-F0-9]{24})$#';
@@ -16,38 +24,38 @@ $bookReviewsPattern = '#^/books/([a-fA-F0-9]{24})/reviews$#';
 // Example: route to controllers (to be implemented)
 // You will expand this switch as you add features
 switch (true) {
-    case $requestUri === '/register' && $requestMethod === 'POST':
+    case $path === '/register' && $requestMethod === 'POST':
         AuthController::register();
         break;
-    case $requestUri === '/login' && $requestMethod === 'POST':
+    case $path === '/login' && $requestMethod === 'POST':
         AuthController::login();
         break;
-    case $requestUri === '/books' && $requestMethod === 'POST':
+    case $path === '/books' && $requestMethod === 'POST':
         $user = AuthMiddleware::authenticate();
         BookController::create($user);
         break;
-    case $requestUri === '/books' && $requestMethod === 'GET':
+    case $path === '/books' && $requestMethod === 'GET':
         BookController::getAll();
         break;
-    case preg_match($bookIdPattern, $requestUri, $matches) && $requestMethod === 'GET':
+    case preg_match($bookIdPattern, $path, $matches) && $requestMethod === 'GET':
         BookController::getOne($matches[1]);
         break;
-    case preg_match($bookIdPattern, $requestUri, $matches) && $requestMethod === 'PUT':
+    case preg_match($bookIdPattern, $path, $matches) && $requestMethod === 'PUT':
         $user = AuthMiddleware::authenticate();
         BookController::update($matches[1], $user);
         break;
-    case preg_match($bookIdPattern, $requestUri, $matches) && $requestMethod === 'DELETE':
+    case preg_match($bookIdPattern, $path, $matches) && $requestMethod === 'DELETE':
         $user = AuthMiddleware::authenticate();
         BookController::delete($matches[1], $user);
         break;
-    case preg_match($bookReviewsPattern, $requestUri, $matches) && $requestMethod === 'POST':
+    case preg_match($bookReviewsPattern, $path, $matches) && $requestMethod === 'POST':
         $user = AuthMiddleware::authenticate();
         ReviewController::add($matches[1], $user);
         break;
-    case preg_match($bookReviewsPattern, $requestUri, $matches) && $requestMethod === 'GET':
+    case preg_match($bookReviewsPattern, $path, $matches) && $requestMethod === 'GET':
         ReviewController::getAll($matches[1]);
         break;
-    case preg_match($reviewIdPattern, $requestUri, $matches) && $requestMethod === 'DELETE':
+    case preg_match($reviewIdPattern, $path, $matches) && $requestMethod === 'DELETE':
         $user = AuthMiddleware::authenticate();
         ReviewController::delete($matches[1], $user);
         break;
